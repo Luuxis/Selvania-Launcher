b316df3e5532af51e63d21d333a5ae43a1096cea
MoiAxo <154351069+MoiAxo@users.noreply.github.com>
2025-12-23 13:50:22 +0100

    Add files via upload

---
+++ .github/files/php/instances.php
@@ -0,0 +1,30 @@
+<?php
+$instance['ATM9'] = array_merge($instance['ATM9'], array(
+    "loadder" => array(
+        "minecraft_version" => "1.20.1",
+        "loadder_type" => "forge",
+        "loadder_version" => "latest"
+    ),
+    "verify" => true,
+    "ignored" => array(
+        'config',
+        'essential',
+        'logs',
+        'resourcepacks',
+        'saves',
+        'screenshots',
+        'shaderpacks',
+        'W-OVERFLOW',
+        'options.txt',
+        'optionsof.txt'
+    ),
+    "whitelist" => array(),
+    "whitelistActive" => false,
+    "status" => array(
+        "nameServer" => "ATM9",
+        "ip" => "fecondeurdhomme.aternos.me",
+        "port" => 25565
+    )
+));
+
+?>
