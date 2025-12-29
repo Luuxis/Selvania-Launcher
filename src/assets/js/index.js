4d5c67092779792f4e5066da7b4cfb84d0ae0088
DJDams1 <dami.scoot3@gmail.com>
2025-12-29 18:19:24 +0100

    Chor(home): don't show ms server

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,162 +3,230 @@
  * Luuxis License v1.0 (voir fichier LICENSE pour les détails en FR/EN)
  */
 
-const { ipcRenderer, shell } = require('electron');
-const pkg = require('../package.json');
-const os = require('os');
-import { config, database } from './utils.js';
+const { ipcRenderer, shell } = require("electron");
+const pkg = require("../package.json");
+const os = require("os");
+import { config, database } from "./utils.js";
 const nodeFetch = require("node-fetch");
 
-
 class Splash {
-    constructor() {
-        this.splash = document.querySelector(".splash");
-        this.splashMessage = document.querySelector(".splash-message");
-        this.splashAuthor = document.querySelector(".splash-author");
-        this.message = document.querySelector(".message");
-        this.progress = document.querySelector(".progress");
-        document.addEventListener('DOMContentLoaded', async () => {
-            let databaseLauncher = new database();
-            let configClient = await databaseLauncher.readData('configClient');
-            let theme = configClient?.launcher_config?.theme || "auto"
-            let isDarkTheme = await ipcRenderer.invoke('is-dark-theme', theme).then(res => res)
-            document.body.className = isDarkTheme ? 'dark global' : 'light global';
-            if (process.platform == 'win32') ipcRenderer.send('update-window-progress-load')
-            this.startAnimation()
-        });
-    }
+  constructor() {
+    this.splash = document.querySelector(".splash");
+    this.splashMessage = document.querySelector(".splash-message");
+    this.splashAuthor = document.querySelector(".splash-author");
+    this.message = document.querySelector(".message");
+    this.progress = document.querySelector(".progress");
+    document.addEventListener("DOMContentLoaded", async () => {
+      let databaseLauncher = new database();
+      let configClient = await databaseLauncher.readData("configClient");
+      let theme = configClient?.launcher_config?.theme || "auto";
+      let isDarkTheme = await ipcRenderer
+        .invoke("is-dark-theme", theme)
+        .then((res) => res);
+      document.body.className = isDarkTheme ? "dark global" : "light global";
+      if (process.platform == "win32")
+        ipcRenderer.send("update-window-progress-load");
+      this.startAnimation();
+    });
+  }
 
-    async startAnimation() {
-        let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
-        ];
-        let splash = splashes[Math.floor(Math.random() * splashes.length)];
-        this.splashMessage.textContent = splash.message;
-        this.splashAuthor.children[0].textContent = "@" + splash.author;
-        await sleep(100);
-        document.querySelector("#splash").style.display = "block";
-        await sleep(500);
-        this.splash.classList.add("opacity");
-        await sleep(500);
-        this.splash.classList.add("translate");
-        this.splashMessage.classList.add("opacity");
-        this.splashAuthor.classList.add("opacity");
-        this.message.classList.add("opacity");
-        await sleep(1000);
-        this.checkUpdate();
-    }
+  async startAnimation() {
+    let splashes = [
+      [
+        {
+          message: "Laxacube : Plus de cubes, moins de sommeil.",
+          author: "BlackAngel_TV_",
+        },
+        {
+          message: "Si ça lag, c'est que c'est une feature.",
+          author: "Zarroc",
+        },
+        {
+          message: "Le premier qui trouve un bug gagne... ma reconnaissance.",
+          author: "BlackAngel_TV_",
+        },
+        {
+          message:
+            "On ne dit pas 'Anti-Cheat', on dit 'Arbitre de la physique'.",
+          author: "Laxacube",
+        },
+        {
+          message: "Manger, dormir, Laxacube, recommencer.",
+          author: "BlackAngel_TV_",
+        },
+        {
+          message: "Le diamant, c'est surfait. Vive la Netherite.",
+          author: "Laxacube",
+        },
+        {
+          message: "Pourquoi sortir quand on peut miner chez nous ?",
+          author: "BlackAngel_TV_",
+        },
+        {
+          message: "Chargement des chunks... et de vos espoirs.",
+          author: "Console",
+        },
+        {
+          message: "Laxacube ne crash pas, il fait une pause méditative.",
+          author: "BlackAngel_TV_",
+        },
+      ],
+    ];
+    let splash = splashes[Math.floor(Math.random() * splashes.length)];
+    this.splashMessage.textContent = splash.message;
+    this.splashAuthor.children[0].textContent = "@" + splash.author;
+    await sleep(100);
+    document.querySelector("#splash").style.display = "block";
+    await sleep(500);
+    this.splash.classList.add("opacity");
+    await sleep(500);
+    this.splash.classList.add("translate");
+    this.splashMessage.classList.add("opacity");
+    this.splashAuthor.classList.add("opacity");
+    this.message.classList.add("opacity");
+    await sleep(1000);
+    this.checkUpdate();
+  }
 
-    async checkUpdate() {
-        this.setStatus(`Recherche de mise à jour...`);
+  async checkUpdate() {
+    this.setStatus(`Recherche de mise à jour...`);
 
-        ipcRenderer.invoke('update-app').then().catch(err => {
-            return this.shutdown(`erreur lors de la recherche de mise à jour :<br>${err.message}`);
-        });
+    ipcRenderer
+      .invoke("update-app")
+      .then()
+      .catch((err) => {
+        return this.shutdown(
+          `erreur lors de la recherche de mise à jour :<br>${err.message}`
+        );
+      });
 
-        ipcRenderer.on('updateAvailable', () => {
-            this.setStatus(`Mise à jour disponible !`);
-            if (os.platform() == 'win32') {
-                this.toggleProgress();
-                ipcRenderer.send('start-update');
-            }
-            else return this.dowloadUpdate();
-        })
+    ipcRenderer.on("updateAvailable", () => {
+      this.setStatus(`Mise à jour disponible !`);
+      if (os.platform() == "win32") {
+        this.toggleProgress();
+        ipcRenderer.send("start-update");
+      } else return this.dowloadUpdate();
+    });
 
-        ipcRenderer.on('error', (event, err) => {
-            if (err) return this.shutdown(`${err.message}`);
-        })
+    ipcRenderer.on("error", (event, err) => {
+      if (err) return this.shutdown(`${err.message}`);
+    });
 
-        ipcRenderer.on('download-progress', (event, progress) => {
-            ipcRenderer.send('update-window-progress', { progress: progress.transferred, size: progress.total })
-            this.setProgress(progress.transferred, progress.total);
-        })
+    ipcRenderer.on("download-progress", (event, progress) => {
+      ipcRenderer.send("update-window-progress", {
+        progress: progress.transferred,
+        size: progress.total,
+      });
+      this.setProgress(progress.transferred, progress.total);
+    });
 
-        ipcRenderer.on('update-not-available', () => {
-            console.error("Mise à jour non disponible");
-            this.maintenanceCheck();
-        })
-    }
+    ipcRenderer.on("update-not-available", () => {
+      console.error("Mise à jour non disponible");
+      this.maintenanceCheck();
+    });
+  }
 
-    getLatestReleaseForOS(os, preferredFormat, asset) {
-        return asset.filter(asset => {
-            const name = asset.name.toLowerCase();
-            const isOSMatch = name.includes(os);
-            const isFormatMatch = name.endsWith(preferredFormat);
-            return isOSMatch && isFormatMatch;
-        }).sort((a, b) => new Date(b.created_at) - new Date(a.created_at))[0];
-    }
+  getLatestReleaseForOS(os, preferredFormat, asset) {
+    return asset
+      .filter((asset) => {
+        const name = asset.name.toLowerCase();
+        const isOSMatch = name.includes(os);
+        const isFormatMatch = name.endsWith(preferredFormat);
+        return isOSMatch && isFormatMatch;
+      })
+      .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))[0];
+  }
 
-    async dowloadUpdate() {
-        const repoURL = pkg.repository.url.replace("git+", "").replace(".git", "").replace("https://github.com/", "").split("/");
-        const githubAPI = await nodeFetch('https://api.github.com').then(res => res.json()).catch(err => err);
+  async dowloadUpdate() {
+    const repoURL = pkg.repository.url
+      .replace("git+", "")
+      .replace(".git", "")
+      .replace("https://github.com/", "")
+      .split("/");
+    const githubAPI = await nodeFetch("https://api.github.com")
+      .then((res) => res.json())
+      .catch((err) => err);
 
-        const githubAPIRepoURL = githubAPI.repository_url.replace("{owner}", repoURL[0]).replace("{repo}", repoURL[1]);
-        const githubAPIRepo = await nodeFetch(githubAPIRepoURL).then(res => res.json()).catch(err => err);
+    const githubAPIRepoURL = githubAPI.repository_url
+      .replace("{owner}", repoURL[0])
+      .replace("{repo}", repoURL[1]);
+    const githubAPIRepo = await nodeFetch(githubAPIRepoURL)
+      .then((res) => res.json())
+      .catch((err) => err);
 
-        const releases_url = await nodeFetch(githubAPIRepo.releases_url.replace("{/id}", '')).then(res => res.json()).catch(err => err);
-        const latestRelease = releases_url[0].assets;
-        let latest;
+    const releases_url = await nodeFetch(
+      githubAPIRepo.releases_url.replace("{/id}", "")
+    )
+      .then((res) => res.json())
+      .catch((err) => err);
+    const latestRelease = releases_url[0].assets;
+    let latest;
 
-        if (os.platform() == 'darwin') latest = this.getLatestReleaseForOS('mac', '.dmg', latestRelease);
-        else if (os == 'linux') latest = this.getLatestReleaseForOS('linux', '.appimage', latestRelease);
+    if (os.platform() == "darwin")
+      latest = this.getLatestReleaseForOS("mac", ".dmg", latestRelease);
+    else if (os == "linux")
+      latest = this.getLatestReleaseForOS("linux", ".appimage", latestRelease);
 
+    this.setStatus(
+      `Mise à jour disponible !<br><div class="download-update">Télécharger</div>`
+    );
+    document.querySelector(".download-update").addEventListener("click", () => {
+      shell.openExternal(latest.browser_download_url);
+      return this.shutdown("Téléchargement en cours...");
+    });
+  }
 
-        this.setStatus(`Mise à jour disponible !<br><div class="download-update">Télécharger</div>`);
-        document.querySelector(".download-update").addEventListener("click", () => {
-            shell.openExternal(latest.browser_download_url);
-            return this.shutdown("Téléchargement en cours...");
-        });
-    }
+  async maintenanceCheck() {
+    config
+      .GetConfig()
+      .then((res) => {
+        if (res.maintenance) return this.shutdown(res.maintenance_message);
+        this.startLauncher();
+      })
+      .catch((e) => {
+        console.error(e);
+        return this.shutdown(
+          "Aucune connexion internet détectée,<br>veuillez réessayer ultérieurement."
+        );
+      });
+  }
 
+  startLauncher() {
+    this.setStatus(`Démarrage du launcher`);
+    ipcRenderer.send("main-window-open");
+    ipcRenderer.send("update-window-close");
+  }
 
-    async maintenanceCheck() {
-        config.GetConfig().then(res => {
-            if (res.maintenance) return this.shutdown(res.maintenance_message);
-            this.startLauncher();
-        }).catch(e => {
-            console.error(e);
-            return this.shutdown("Aucune connexion internet détectée,<br>veuillez réessayer ultérieurement.");
-        })
-    }
+  shutdown(text) {
+    this.setStatus(`${text}<br>Arrêt dans 5s`);
+    let i = 4;
+    setInterval(() => {
+      this.setStatus(`${text}<br>Arrêt dans ${i--}s`);
+      if (i < 0) ipcRenderer.send("update-window-close");
+    }, 1000);
+  }
 
-    startLauncher() {
-        this.setStatus(`Démarrage du launcher`);
-        ipcRenderer.send('main-window-open');
-        ipcRenderer.send('update-window-close');
-    }
+  setStatus(text) {
+    this.message.innerHTML = text;
+  }
 
-    shutdown(text) {
-        this.setStatus(`${text}<br>Arrêt dans 5s`);
-        let i = 4;
-        setInterval(() => {
-            this.setStatus(`${text}<br>Arrêt dans ${i--}s`);
-            if (i < 0) ipcRenderer.send('update-window-close');
-        }, 1000);
-    }
+  toggleProgress() {
+    if (this.progress.classList.toggle("show")) this.setProgress(0, 1);
+  }
 
-    setStatus(text) {
-        this.message.innerHTML = text;
-    }
-
-    toggleProgress() {
-        if (this.progress.classList.toggle("show")) this.setProgress(0, 1);
-    }
-
-    setProgress(value, max) {
-        this.progress.value = value;
-        this.progress.max = max;
-    }
+  setProgress(value, max) {
+    this.progress.value = value;
+    this.progress.max = max;
+  }
 }
 
 function sleep(ms) {
-    return new Promise(r => setTimeout(r, ms));
+  return new Promise((r) => setTimeout(r, ms));
 }
 
 document.addEventListener("keydown", (e) => {
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 73 || e.keyCode == 123) {
-        ipcRenderer.send("update-window-dev-tools");
-    }
-})
+  if ((e.ctrlKey && e.shiftKey && e.keyCode == 73) || e.keyCode == 123) {
+    ipcRenderer.send("update-window-dev-tools");
+  }
+});
 new Splash();
